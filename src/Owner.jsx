3a2af0a2c71467b4e0795eb770954d9f44e092aa
Wichy007 <naravitdansiriviroj@gmail.com>
2023-04-19 10:11:54 +0700

    fix my_photo

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Layout from "./Layout";
+import myPhoto from "./my_photo.jpg"; 
 
 const Owner = () => {
     return (
         <Layout>
             <div style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
                 <h1>Hello I'm Wichy No.23 Form Project H</h1>
-                <img src="src\my _photo.jpg" style={{width:'300px'}}/>
+                <img src={myPhoto} style={{width:'300px'}}/>
                 <h2>Short Biography</h2>
                 <p>Hello again, My nmae is Naravit Bunthap. My nickname is vit. You can call me vit or Wichy.
                 I'm graduated Bachelor degree at Mahidol usiversity faculty Medical Technology
